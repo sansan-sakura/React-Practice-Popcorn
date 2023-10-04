0018a66756269999a11b5ed76ba8835abc5850aa
Sakura Tanaka <sito6496t@google.com>
2023-10-04 14:42:51 +0200

    movie detail part is added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App_v1";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10} />
-    <StarRating />
-    {/* <App /> */}
+    {/* <StarRating maxRating={10} /> */}
+    {/* <StarRating /> */}
+    <App />
   </React.StrictMode>
 );
